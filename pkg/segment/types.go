21ba2eff6f5b317d924be99857c08a7f63ca6cca
Scott Hawkins <scott@echoboomer.net>
2023-04-14 13:33:07 -0400

    feat: segment add metrics (#10)
    * feat: segment add metrics
    
    * update metric names

--- pkg/segment/types.go
+++ pkg/segment/types.go
@@ -14,11 +14,33 @@ import (
 // SegmentIOWriteKey The write key is the unique identifier for a source that tells Segment which source data comes
 // from, to which workspace the data belongs, and which destinations should receive the data.
 const (
-	SegmentIOWriteKey                 = "0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M"
-	MetricInitStarted                 = "kubefirst.init.started"
-	MetricInitCompleted               = "kubefirst.init.completed"
-	MetricMgmtClusterInstallStarted   = "kubefirst.mgmt_cluster_install.started"
-	MetricMgmtClusterInstallCompleted = "kubefirst.mgmt_cluster_install.completed"
+	SegmentIOWriteKey = "0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M"
+
+	MetricInitStarted                    = "kubefirst.init.started"
+	MetricInitCompleted                  = "kubefirst.init.completed"
+	MetricCloudCredentialsCheckStarted   = "kubefirst.init.cloud_credentials_check.started"
+	MetricCloudCredentialsCheckCompleted = "kubefirst.init.cloud_credentials_check.completed"
+	MetricKbotSetupStarted               = "kubefirst.init.kbot_setup.started"
+	MetricKbotSetupCompleted             = "kubefirst.init.kbot_setup.completed"
+	MetricMgmtClusterInstallStarted      = "kubefirst.mgmt_cluster_install.started"
+	MetricMgmtClusterInstallCompleted    = "kubefirst.mgmt_cluster_install.completed"
+
+	MetricGitTerraformApplyStarted     = "kubefirst.git_terraform_apply.started"
+	MetricGitTerraformApplyCompleted   = "kubefirst.git_terraform_apply.completed"
+	MetricGitopsRepoPushStarted        = "kubefirst.gitops_repo_push.started"
+	MetricGitopsRepoPushCompleted      = "kubefirst.gitops_repo_push.completed"
+	MetricCloudTerraformApplyStarted   = "kubefirst.cloud_terraform_apply.started"
+	MetricCloudTerraformApplyCompleted = "kubefirst.cloud_terraform_apply.completed"
+	MetricArgoCDInstallStarted         = "kubefirst.argocd_install.started"
+	MetricArgoCDInstallCompleted       = "kubefirst.argocd_install.completed"
+	MetricCreateRegistryStarted        = "kubefirst.create_registry.started"
+	MetricCreateRegistryCompleted      = "kubefirst.create_registry.completed"
+	MetricVaultInitializationStarted   = "kubefirst.vault_initialization.started"
+	MetricVaultInitializationCompleted = "kubefirst.vault_initialization.completed"
+	MetricVaultTerraformApplyStarted   = "kubefirst.vault_terraform_apply.started"
+	MetricVaultTerraformApplyCompleted = "kubefirst.vault_terraform_apply.completed"
+	MetricUsersTerraformApplyStarted   = "kubefirst.users_terraform_apply.started"
+	MetricUsersTerraformApplyCompleted = "kubefirst.users_terraform_apply.completed"
 )
 
 type SegmentClient struct {
