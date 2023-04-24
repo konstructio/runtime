d83a424cd35b4e1e074d75e93d4f74c3ea0614e9
Scott Hawkins <scott@echoboomer.net>
2023-04-23 21:24:47 -0400

    feat: dns verification for cloud providers (#18)
    * feat: dns verification for cloud providers
    
    * add aws

---
+++ pkg/dns/dns.go
@@ -0,0 +1,79 @@
+/*
+Copyright (C) 2021-2023, Kubefirst
+
+This program is licensed under MIT.
+See the LICENSE file for more details.
+*/
+package dns
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kubefirst/runtime/pkg"
+	awsinternal "github.com/kubefirst/runtime/pkg/aws"
+	"github.com/lixiangzhong/dnsutil"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	// Google
+	dnsLookupHost string = "8.8.8.8"
+)
+
+var (
+	CivoNameServers         []string = []string{"ns0.civo.com", "ns1.civo.com"}
+	DigitalOceanNameServers []string = []string{"ns1.digitalocean.com", "ns2.digitalocean.com", "ns3.digitalocean.com"}
+	VultrNameservers        []string = []string{"ns1.vultr.com", "ns2.vultr.com"}
+)
+
+// VerifyProviderDNS
+func VerifyProviderDNS(cloudProvider string, cloudRegion string, domainName string) error {
+	var dig dnsutil.Dig
+	var nameServers []string
+	dig.SetDNS(dnsLookupHost)
+
+	switch cloudProvider {
+	case "aws":
+		awsClient := &awsinternal.AWSConfiguration{
+			Config: awsinternal.NewAwsV2(cloudRegion),
+		}
+		hostedZoneID, err := awsClient.GetHostedZoneID(domainName)
+		if err != nil {
+			return err
+		}
+		hostedZone, err := awsClient.GetHostedZone(hostedZoneID)
+		if err != nil {
+			return err
+		}
+		nameServers = hostedZone.DelegationSet.NameServers
+	case "civo":
+		nameServers = CivoNameServers
+	case "digitalocean":
+		nameServers = DigitalOceanNameServers
+	case "vultr":
+		nameServers = VultrNameservers
+	default:
+		return fmt.Errorf("unsupported cloud provider for dns verification: %s", cloudProvider)
+	}
+
+	records, err := dig.NS(domainName)
+	if err != nil {
+		return fmt.Errorf("error checking NS record for domain %s: %s", domainName, err)
+	}
+
+	var foundNSRecords []string
+	for _, rec := range records {
+		foundNSRecords = append(foundNSRecords, strings.TrimSuffix(rec.Ns, "."))
+	}
+
+	for _, reqrec := range nameServers {
+		if pkg.FindStringInSlice(foundNSRecords, reqrec) {
+			log.Info().Msgf("found NS record %s for domain %s", reqrec, domainName)
+		} else {
+			return fmt.Errorf("missing record %s for domain %s - please add the NS record", reqrec, domainName)
+		}
+	}
+
+	return nil
+}
