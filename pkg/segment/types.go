46c87439eb86ca1967a205d0805ff9d2d9d43b9f
CristhianF7 <CristhianF7@gmail.com>
2023-06-05 13:42:33 -0500

    feat: telemetry updates

--- pkg/segment/types.go
+++ pkg/segment/types.go
@@ -16,6 +16,9 @@ import (
 const (
 	SegmentIOWriteKey = "0gAYkX5RV3vt7s4pqCOOsDb6WHPLT30M"
 
+	// Install
+	MetricKubefirstInstalled = "kubefirst.installed"
+
 	// Init
 	MetricInitStarted   = "kubefirst.init.started"
 	MetricInitCompleted = "kubefirst.init.completed"
@@ -43,6 +46,8 @@ const (
 	// Create
 	MetricMgmtClusterInstallStarted   = "kubefirst.mgmt_cluster_install.started"
 	MetricMgmtClusterInstallCompleted = "kubefirst.mgmt_cluster_install.completed"
+	MetricClusterInstallStarted   = "kubefirst.cluster_install.started"
+	MetricClusterInstallCompleted = "kubefirst.cluster_install.completed"
 
 	MetricGitTerraformApplyStarted   = "kubefirst.git_terraform_apply.started"
 	MetricGitTerraformApplyCompleted = "kubefirst.git_terraform_apply.completed"
@@ -79,6 +84,8 @@ const (
 	// Delete
 	MetricMgmtClusterDeleteStarted   = "kubefirst.mgmt_cluster_delete.started"
 	MetricMgmtClusterDeleteCompleted = "kubefirst.mgmt_cluster_delete.completed"
+	MetricClusterDeleteStarted   = "kubefirst.cluster_delete.started"
+	MetricClusterDeleteCompleted = "kubefirst.cluster_delete.completed"
 )
 
 type SegmentClient struct {
@@ -89,6 +96,7 @@ type SegmentClient struct {
 	ClusterType       string
 	DomainName        string
 	GitProvider       string
+	InstallMethod			string
 	KubefirstClient   string
 	KubefirstTeam     string
 	KubefirstTeamInfo string
