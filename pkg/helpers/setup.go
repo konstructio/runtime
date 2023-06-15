24a06a4ad9369e1c6db868d12e1bcf6515555673
Scott Hawkins <scott@echoboomer.net>
2023-06-15 14:30:21 -0400

    fix: return what can be found for root-creds instead of exiting (#67)

--- pkg/helpers/setup.go
+++ pkg/helpers/setup.go
@@ -22,17 +22,17 @@ func DisplayLogHints() {
 	fmt.Println(strings.Repeat("-", 48))
 }
 
-// GetCompletionFlags gets specific config flags to mark status of an install
-func GetCompletionFlags() CompletionFlags {
-	return CompletionFlags{
+// GetClusterStatusFlags gets specific config flags to mark status of an install
+func GetClusterStatusFlags() ClusterStatusFlags {
+	return ClusterStatusFlags{
 		CloudProvider: viper.GetString("kubefirst.cloud-provider"),
 		GitProvider:   viper.GetString("kubefirst.git-provider"),
 		SetupComplete: viper.GetBool("kubefirst.setup-complete"),
 	}
 }
 
-// SetCompletionFlags sets specific config flags to mark status of an install
-func SetCompletionFlags(cloudProvider string, gitProvider string) {
+// SetClusterStatusFlags sets specific config flags to mark status of an install
+func SetClusterStatusFlags(cloudProvider string, gitProvider string) {
 	viper.Set("kubefirst.cloud-provider", cloudProvider)
 	viper.Set("kubefirst.git-provider", gitProvider)
 	viper.Set("kubefirst.setup-complete", true)
