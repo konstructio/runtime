3d08924a1ab206bfe370ffd4f32001e6d1050e5a
Scott Hawkins <scott@echoboomer.net>
2023-06-21 12:24:28 -0400

    fix: gitlab client pagination

--- pkg/k8s/wrappers.go
+++ pkg/k8s/wrappers.go
@@ -13,7 +13,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 // OpenPortForwardPodWrapper wrapper for PortForwardPod function. This functions make it easier to open and close port
@@ -83,64 +82,4 @@ func OpenPortForwardPodWrapper(
 
 	log.Info().Msgf("pod %q at namespace %q has port-forward accepting local connections at port %d\n", podName, namespace, podLocalPort)
 
-}
-
-func OpenPortForwardServiceWrapper(
-	kubeconfigPath string,
-	kubeconfigClientPath string,
-	namespace string,
-	serviceName string,
-	servicePort int,
-	serviceLocalPort int,
-	stopChannel chan struct{},
-) {
-	kubeconfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-	if err != nil {
-		log.Error().Err(err).Msg("")
-	}
-
-	// readyCh communicate when the port forward is ready to get traffic
-	readyCh := make(chan struct{})
-
-	portForwardRequest := PortForwardAServiceRequest{
-		RestConfig: kubeconfig,
-		Service: v1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      serviceName,
-				Namespace: namespace,
-			},
-		},
-		ServicePort: servicePort,
-		LocalPort:   serviceLocalPort,
-		StopCh:      stopChannel,
-		ReadyCh:     readyCh,
-	}
-
-	clientset, _ := GetClientSet(kubeconfigPath)
-
-	// Check to see if the port is already used
-	err = CheckForExistingPortForwards(serviceLocalPort)
-	if err != nil {
-		log.Fatal().Msgf("unable to start port forward for service %s in namespace %s: %s", serviceName, namespace, err)
-	}
-
-	go func() {
-		// todo, i think we can use the RestConfig and remove the "kubectlClientPath"
-		err = PortForwardService(clientset, kubeconfigPath, kubeconfigClientPath, portForwardRequest)
-		if err != nil {
-			log.Error().Err(err).Msg("")
-		}
-	}()
-
-	select {
-	case <-stopChannel:
-		log.Info().Msg("leaving...")
-		close(stopChannel)
-		close(readyCh)
-		break
-	case <-readyCh:
-		log.Info().Msg("port forwarding is ready to get traffic")
-	}
-
-	log.Info().Msgf("service %q at namespace %q has port-forward accepting local connections at port %d\n", serviceName, namespace, serviceLocalPort)
 }
