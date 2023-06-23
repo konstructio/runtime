034336eb5b345812f96afe95e965001d3ed444f6
Scott Hawkins <scott@echoboomer.net>
2023-06-23 17:12:49 -0400

    feat: add func to eval ssh key centrally (#76)

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -9,16 +9,19 @@ package ssh
 import (
 	"crypto/rand"
 	"encoding/pem"
+	"fmt"
+	"strings"
 
 	"github.com/caarlos0/sshmarshal"
 	goGitSsh "github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/kubefirst/runtime/pkg/gitlab"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/ed25519"
 	"golang.org/x/crypto/ssh"
 )
 
 func CreateSshKeyPair() (string, string, error) {
-
 	pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		return "", "", err
@@ -47,4 +50,47 @@ func PublicKeyV2() (*goGitSsh.PublicKeys, error) {
 		return nil, err
 	}
 	return publicKey, err
+}
+
+// EvalSSHKey
+func EvalSSHKey(req *EvalSSHKeyRequest) error {
+	// For GitLab, we currently need to add an ssh key to the authenticating user
+	if req.GitProvider == "gitlab" {
+		gitlabClient, err := gitlab.NewGitLabClient(req.GitToken, req.GitlabGroupFlag)
+		if err != nil {
+			return err
+		}
+		keys, err := gitlabClient.GetUserSSHKeys()
+		if err != nil {
+			log.Fatal().Msgf("unable to check for ssh keys in gitlab: %s", err.Error())
+		}
+
+		var keyName = "kbot-ssh-key"
+		var createKey bool = false
+		for _, key := range keys {
+			if key.Title == keyName {
+				if strings.Contains(key.Key, strings.TrimSuffix(viper.GetString("kbot.public-key"), "\n")) {
+					log.Info().Msgf("ssh key %s already exists and key is up to date, continuing", keyName)
+				} else {
+					log.Fatal().Msgf("ssh key %s already exists and key data has drifted - it will be recreated", keyName)
+					err := gitlabClient.DeleteUserSSHKey(keyName)
+					if err != nil {
+						return fmt.Errorf("error deleting gitlab user ssh key %s: %s", keyName, err)
+					}
+					createKey = true
+				}
+			}
+		}
+		if createKey {
+			log.Info().Msgf("creating ssh key %s...", keyName)
+			err := gitlabClient.AddUserSSHKey(keyName, viper.GetString("kbot.public-key"))
+			if err != nil {
+				log.Fatal().Msgf("error adding ssh key %s: %s", keyName, err.Error())
+			}
+			viper.Set("kbot.gitlab-user-based-ssh-key-title", keyName)
+			viper.WriteConfig()
+		}
+	}
+
+	return nil
 }
