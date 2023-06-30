24893630797b325d067215768e1c182ea653ea12
Scott Hawkins <scott@echoboomer.net>
2023-06-30 13:36:39 -0400

    fix: close listen for pf on err (#81)
    * fix: close listen for pf on err
    
    * other fixes
    
    * better

--- pkg/k8s/helpers.go
+++ pkg/k8s/helpers.go
@@ -16,6 +16,7 @@ func CheckForExistingPortForwards(ports ...int) error {
 	for _, port := range ports {
 		listen, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", port))
 		if err != nil {
+			_ = listen.Close()
 			return fmt.Errorf("port %v is in use", port)
 		}
 		_ = listen.Close()
